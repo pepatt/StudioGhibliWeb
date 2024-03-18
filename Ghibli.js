5de311e6e4c03b970d9a44b43ccb6049a3c8a3b0
Tymur Rezaei <rezai.timur.04@gmail.com>
2024-03-17 22:59:56 -0700

    Update Ghibli.js

--- Ghibli.js
+++ Ghibli.js
@@ -5,3 +5,15 @@ function updateBodyHeight() {
     body.style.height = height + 'px';
 }
 updateBodyHeight();
+
+function printRadio(radioName){
+    var radio = document.getElementsByName(radioName);
+    var output = document.getElementById('output');
+    for(var i = 0; i < radio.length; i++) { 
+        if(radio[i].checked) {
+            var newOutput = radio[i].value + ' ||| ' + radio[i].name;
+            output.innerText += newOutput + '\n';
+            break;
+        }
+    }
+}
